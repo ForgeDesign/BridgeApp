6ae3347fd428c1b5d3cc32f280a1e6cbf183063d
unknown <cjframe1@gmail.com>
2017-12-20 15:15:28 -0500

    Mostly finihsed iso screen

---
+++ app/components/PersonCard/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { View } from 'react-native';
+import PersonCard from './PersonCard';
+import styles from './styles';
+export { PersonCard, styles };
--- app/screens/IsoScreen.js
+++ app/screens/IsoScreen.js
@@ -1,16 +1,67 @@
 import React, { Component } from 'react';
-import { View, Text, AppRegistry } from 'react-native';
+import { View, Text, AppRegistry, ScrollView } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-import { Navbar } from '../components/Navbar';
+import { Container } from '../components/Container';
+import { Header } from '../components/Header';
+import { PersonCard } from '../components/PersonCard';
+
+var people = [
+  {
+    "name": "George Washington",
+    "location": "Westmoreland, VA",
+    "imagepath": "../assets/images/george.jpg"
+  },
+  {
+    "name": 'Thomas Jefferson',
+    "location": 'Shadwell, VA',
+    "imagepath": '../assets/images/tommy.jpg'
+  }
+]
 
 export default class IsoScreen extends React.Component {
+
+
+  constructor(){
+    super();
+    this.state = {
+      people:
+        [
+          {
+            "name": "George Washington",
+            "location": "Westmoreland, VA",
+            "imagepath": "../assets/images/george.jpg"
+          },
+          {
+            "name": 'Thomas Jefferson',
+            "location": 'Shadwell, VA',
+            "imagepath": '../assets/images/tommy.jpg'
+          }
+        ]
+      }
+  }
 
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Text>Iso</Text>
-      </View>
+      <Container>
+        <Header title={'Connect'}/>
+        <ScrollView style={styles.scroll}>
+          {people.map((person) =>
+            <PersonCard
+              name={person.name}
+              location={person.location}
+              imagepath={person.imagepath}/>
+          )}
+        </ScrollView>
+      </Container>
     )
   }
 }
+
+const styles = EStyleSheet.create({
+  scroll: {
+    flex: 1,
+    backgroundColor: '$offwhite',
+  }
+});
