2375672c65440fdf4137c02339f3fa354116abd5
rodneymorgan97 <rtm36@zips.uakron.edu>
2017-12-20 19:17:50 -0500

    improvements to the icons on the profile page

--- app/components/ProfileHeader/ProfileHeader.js
+++ app/components/ProfileHeader/ProfileHeader.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { View, TouchableOpacity, Text, Icon } from 'react-native';
+import { Icon } from 'native-base';
+import { View, TouchableOpacity, Text } from 'react-native';
 import styles from './styles';
 
 
 const ProfileHeader = () => (
     <View style={styles.background}>
+        <View style={styles.threeIcons}>
+            <View style={styles.icon}>
+                <Icon name={'md-share'} size={26} style={{ color: '#fff' }}/>
+            </View>
 
+            <View style={styles.profileIcon}>
+
+            </View>
+
+            <View style={styles.icon}>
+                <Icon name={'md-card'} size={26} style={{ color: '#fff' }}/>
+            </View>
+        </View>
     </View>
 );
 
--- app/components/ProfileHeader/styles.js
+++ app/components/ProfileHeader/styles.js
@@ -1,9 +1,35 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { Dimensions } from 'react-native';
+
+const circleWidth = Dimensions.get('window').width / 5;
 
 export default EStyleSheet.create({
     background: {
       flexDirection: 'row',
-      height: '50%',
+      height: '40%',
       backgroundColor: '$primaryBlue',
+    },
+    threeIcons: {
+      padding: '10%',
+      flexDirection: 'row',
+      alignItems: 'baseline',
+      justifyContent: 'center'
+    },
+    icon: {
+      height: circleWidth,
+      width: circleWidth,
+      borderRadius: circleWidth / 2,
+      borderWidth: 2,
+      borderColor: '#124C67',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    profileIcon:{
+      height: circleWidth * 1.5,
+      width: circleWidth * 1.5,
+      borderRadius: (circleWidth * 1.5) / 2,
+      backgroundColor: '#000',
+      alignItems: 'center'
     }
 });
