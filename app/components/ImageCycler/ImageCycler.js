c43001bad3276bd42d1b81c77915f619ebf55afe
agates4 <agate97@gmail.com>
2017-12-22 18:01:56 -0500

    added image cycler height constraint

--- app/components/ImageCycler/ImageCycler.js
+++ app/components/ImageCycler/ImageCycler.js
@@ -9,23 +9,29 @@ import {
 import styles from './styles';
 import Swiper from 'react-native-swiper';
 
-const ImageCycler = () => (
-        <Swiper style={styles.wrapper} showsButtons={true}>
-            <View style={styles.slide1}>
-                <Image style={styles.card} 
-                
-                resizeMode="contain"
+const ImageCycler = ({height = "40.25%"}) => {
+    
+    return (
+        <View height={height}>
+            <Swiper style={styles.wrapper} showsButtons={true}>
+                <View style={styles.slide1}>
+                    <Image style={styles.card} 
+                    
+                    resizeMode="contain"
 
-                source={require('../../data/CardTemplates/template1.jpg')} />  
-            </View>
-            <View style={styles.slide2}>
-                <Text style={styles.text}>Beautiful</Text>
-            </View>
-            <View style={styles.slide3}>
-                <Text style={styles.text}>And simple</Text>
-            </View>
-        </Swiper>
-  );
+                    source={require('../../data/CardTemplates/template1.jpg')} />  
+                </View>
+                <View style={styles.slide2}>
+                    <Text style={styles.text}>Beautiful</Text>
+                </View>
+                <View style={styles.slide3}>
+                    <Text style={styles.text}>And simple</Text>
+                </View>
+            </Swiper>
+        </View>
+      )
+        
+};
 
   export default ImageCycler;
   
