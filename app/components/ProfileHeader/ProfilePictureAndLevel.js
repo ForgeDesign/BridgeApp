92b62af48fad59c4f1c813f9ac8bcda56253fc09
rodneymorgan97 <rtm36@zips.uakron.edu>
2018-01-11 14:51:38 -0500

    merge develop, async storage doesn't work properly

--- app/components/ProfileHeader/ProfilePictureAndLevel.js
+++ app/components/ProfileHeader/ProfilePictureAndLevel.js
@@ -21,9 +21,9 @@ export default class ProfilePictureAndLevel extends React.Component
 
         this.state = 
         {
-            profilePic: "../../assets/images/black.jpg"
+            profilePic: ''
         };
-        this.addProfilePic = this.addProfilePic.bind(this);
+        
     }
 
     options = 
@@ -38,6 +38,26 @@ export default class ProfilePictureAndLevel extends React.Component
         }
     };
 
+    componentWillMount() {
+        store.get('profilePic').then((value) => {
+          if (value!==null){
+          this.setState({profilePic: value.profilePic});
+          this.forceUpdate();
+          }
+          else{
+              this.setState({profilePic: '../../assets/images/jamessmith.jpg'})
+          }
+        });
+      }
+
+      _onRefresh() {
+        store.get('profilePic').then((value) => {
+          if (value!==null){
+            this.setState({profilePic: value.profilePic});
+            this.forceUpdate();
+          }
+        });
+      }
 
     async addProfilePic() {
         await ImagePicker.showImagePicker(this.options, (response) => {
@@ -53,7 +73,6 @@ export default class ProfilePictureAndLevel extends React.Component
             }
             else {
 
-                // You can also display the image using data:
                 let source = response.uri;
 
 
@@ -63,23 +82,14 @@ export default class ProfilePictureAndLevel extends React.Component
                     profilePic: this.state.profilePic
                 }
                 store.push('profilePic', obj);
+                
             }
         })
     }
 
-    _onRefresh() {
-        this.setState({refreshing: true});
-        store.get('profilePic').then((value) => {
-          if (value!==null){
-            this.setState({profilePic: value});
-            this.forceUpdate();
-          }
-          this.setState({refreshing: false});
-        });
-      }
+    
 
     render(){
-        
         return(
                 <TouchableOpacity  onPress={this.addProfilePic} style={{alignItems:'center', justifyContent: 'center', flexDirection:'column'}}>
                     
